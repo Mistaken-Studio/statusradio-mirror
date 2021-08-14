ddfae90a02df3f45a29cd72fa5d1154ececc183c
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-08-14 13:11:47 +0200

    Changed default AutoUpdateConfig value

--- StatusRadio/Config.cs
+++ StatusRadio/Config.cs
@@ -23,6 +23,12 @@ namespace Mistaken.StatusRadio
 
         /// <inheritdoc/>
         [Description("Auto Update Settings")]
-        public System.Collections.Generic.Dictionary<string, string> AutoUpdateConfig { get; set; }
+        public System.Collections.Generic.Dictionary<string, string> AutoUpdateConfig { get; set; } = new System.Collections.Generic.Dictionary<string, string>
+        {
+            { "Url", "https://git.mistaken.pl/api/v4/projects/21" },
+            { "Token", string.Empty },
+            { "Type", "GITLAB" },
+            { "VerbouseOutput", "false" },
+        };
     }
 }
